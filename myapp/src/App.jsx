e15ab4db350614c99d052dd0e46cbbf5c44606fe
Tsedniya <ktsedniya@gmail.com>
2025-11-03 23:18:08 +0300

    frontend:navbar

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
 
-function App() {
+const App=() =>{
  
-
   return (
-    <>
+    < >
       <Navbar />
+
+      <Routes>
+
+        <Route path='/' element={Home}/>
+        <Route path='/contact' element={Contact}/>
+
+      </Routes>
     </>
   )
 }
--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import {Link,NavLink} from 'react-router-dom'
+import logo from '../assets/logo.svg'
+
 
 const Navbar = () => {
   return (
-    <div>
-         
+    <div className="flex items-center justify-between my-5 mx-10 rounded-full bg-amber-900 px-6 py-3">
+  
+  <img className="w-10 h-auto" src={logo} alt="Logo" />
 
-    </div>
+  
+  <ul className="flex justify-center gap-6 text-sm text-gray-200 font-medium">
+    <NavLink to="/" className="hover:text-amber-300">Home</NavLink>
+    <NavLink to="/about" className="hover:text-amber-300">About</NavLink>
+    <NavLink to="/services" className="hover:text-amber-300">Services</NavLink>
+    <NavLink to="/contact" className="hover:text-amber-300">Contact</NavLink>
+    <NavLink to="/about" className="hover:text-amber-300">About</NavLink>
+    <NavLink to="/services" className="hover:text-amber-300">Services</NavLink>
+    <NavLink to="/contact" className="hover:text-amber-300">Contact</NavLink>
+
+  </ul>
+
+  {/* Button */}
+  <button className="bg-white text-amber-900 px-4 py-2 rounded-full font-semibold hover:bg-amber-300 transition">
+    App
+  </button>
+</div>
+
   )
 }
 
