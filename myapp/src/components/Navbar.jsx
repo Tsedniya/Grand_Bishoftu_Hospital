3d24652ee5a6b41afd0743489287b70b497627dd
Tsedniya <ktsedniya@gmail.com>
2025-11-05 22:19:26 +0300

    frontend:hero

--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -6,30 +6,30 @@ import { Hospital } from 'lucide-react';
 
 const Navbar = () => {
   return (
-  <nav className="fixed top-0 left-0 right-0 mx-7 my-5 flex items-center justify-between px-6 py-3 bg-white rounded-full shadow-md z-50">
+  <nav className="fixed top-0 left-0 right-0 mx-13 my-5 flex items-center justify-between px-6 py-3 bg-white rounded-full shadow-md z-50">
   <div className="flex items-center space-x-2">
-    <Hospital className="h-8 w-8 text-blue-600" />
-    <span className="text-2xl font-bold bg-gradient-to-r from-sky-400 to-blue-600 bg-clip-text text-transparent">
+    <Hospital className="h-8 w-8 text-sky-400" />
+    <span className="text-2xl  text-gray-700">
       Grand Bishoftu Hospital
     </span>
   </div>
 
   <ul className="flex gap-6 text-gray-700 font-medium">
-    <NavLink to="/" className="text-gray-700 hover:text-sky-400 transition-colors">
+    <NavLink to="/" className="mx-auto hover:text-sky-400 hover:font-bold text-gray-700  transition-colors">
         <p>Home</p>
-        <hr className='w-13 border-none h-[1.5px] bg-sky-400 hidden'/>
+        <hr className='mx-auto w-10 border-none h-[1.5px] bg-sky-400 hidden'/>
     </NavLink>
-    <NavLink to="/about" className="text-gray-700 hover:text-sky-400  transition-colors">
+    <NavLink to="/about" className="text-gray-700 hover:text-sky-400  hover:font-bold transition-colors">
         <p>About</p>
-        <hr className='w-13 border-none h-[1.5px] bg-sky-400 hidden'/>
+        <hr className='mx-auto w-10 border-none h-[1.5px] bg-sky-400 hidden'/>
     </NavLink>
-    <NavLink to="/services" className="text-gray-700 hover:text-sky-400  transition-colors">
+    <NavLink to="/services" className="text-gray-700 hover:text-sky-400 hover:font-bold transition-colors">
         <p>Services</p>
-        <hr className='w-13 border-none h-[1.5px] bg-sky-400 hidden'/>
+        <hr className='mx-auto w-10 border-none h-[1.5px] bg-sky-400 hidden'/>
     </NavLink>
-    <NavLink to="/contact" className="text-gray-700 hover:text-sky-400  transition-colors">
+    <NavLink to="/contact" className="text-gray-700 hover:text-sky-400 hover:font-bold transition-colors">
         <p>Contact</p>
-        <hr className='w-2/4 border-none h-[1.5px] hover:text-sky-400 hidden'/>
+        <hr className='mx-auto w-10 border-none h-[1.5px] bg-sky-400 hidden'/>
     </NavLink>
   </ul>
 </nav>
