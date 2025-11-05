cd12e8a6064ed22f8eea8d97b2fe7e661ea7c641
Tsedniya <ktsedniya@gmail.com>
2025-11-06 00:04:33 +0300

    frontend:about

--- myapp/src/components/TitleAbout.jsx
+++ myapp/src/components/TitleAbout.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
+import hospital from "../assets/hospital.jpg";
+
 
 const About = () => {
   return (
-    <div >
+    <div>
       <div className='max-w-8xl'>
         <p className='text-center mt-35 text-6xl text-sky-400'>About</p>
         <p className="text-center text-gray-700 mt-2 mx-65 mb-4">Providing trusted healthcare services with compassion and excellence. Our experienced medical specialists are committed to ensuring the well-being of every patient through advanced treatment, personalized care, and a patient-first approach.</p>
       </div>
-        
+      
+      <div className='sm:flex '>
       {/*right*/}
       <div className='flex-col ml-20 mt-14  '>
-        <p >Commited to Excellence in HealthCare</p>
+        <p className='text-gray-700 text-4xl font-bold'>Commited to Excellence in HealthCare</p>
         <div className='max-w-2xl '>
-            <p className='mt-3 text-gray-700 text-justify leading-relaxed'>At Grand Bishoftu Hospital, we are committed to providing exceptional healthcare with compassion and expertise. Our experienced medical specialists work tirelessly to ensure the well-being of every patient, offering advanced treatments, personalized care, and a patient-first approach. </p>
-            <p className='mt-3  text-gray-700 text-justify leading-relaxed'>We strive to create a safe and welcoming environment where health, comfort, and trust come first.Providing trusted healthcare services with compassion and excellence. Our experienced medical specialists are committed to ensuring the well-being of every patient through advanced treatment, personalized care, and a patient-first approach</p>
+            <p className='mt-8 text-gray-700 text-justify leading-relaxed'>At Grand Bishoftu Hospital, we are committed to providing exceptional healthcare with compassion and expertise. Our experienced medical specialists work tirelessly to ensure the well-being of every patient, offering advanced treatments, personalized care, and a patient-first approach. </p>
+            <p className='mt-8 text-gray-700 text-justify leading-relaxed'>We strive to create a safe and welcoming environment where health, comfort, and trust come first.Providing trusted healthcare services with compassion and excellence. Our experienced medical specialists are committed to ensuring the well-being of every patient through advanced treatment, personalized care, and a patient-first approach</p>
         </div>
         </div>
            
         {/*left*/}
         <div>
-           <img/>
+           <img className="ml-8 w-140 mr-14 mt-14 rounded-2xl" src={hospital}/>
+        </div>
         </div>
         
         
