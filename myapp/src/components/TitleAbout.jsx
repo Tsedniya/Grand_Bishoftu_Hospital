614466caf75d7c4cf14cc058a2f4c283ca0339ac
Tsedniya <ktsedniya@gmail.com>
2025-11-07 21:58:39 +0300

    frontend:hero

--- myapp/src/components/TitleAbout.jsx
+++ myapp/src/components/TitleAbout.jsx
@@ -4,36 +4,35 @@ import hospital from "../assets/hospital.jpg";
 
 const About = () => {
   return (
-    <div>
-      <div className='flex-col'>
-        <p className='text-center font-poppins mt-35 text-6xl text-sky-400'>About</p>
-        <p className="text-center text-gray-700 mt-2 mx-65 mb-4">Providing trusted healthcare services with compassion and excellence. Our experienced medical specialists are committed to ensuring the well-being of every patient through advanced treatment, personalized care, and a patient-first approach.</p>
+   <div className="pt-20">
+      <div className='flex flex-col items-center px-4 md:px-16'>
+        <p className='text-center font-poppins text-3xl md:text-6xl text-sky-400 mt-8 md:mt-16'>About</p>
+        <p className="text-center text-gray-700 mt-4 md:mt-6 mx-0 md:mx-16 mb-4">
+          Providing trusted healthcare services with compassion and excellence. Our experienced medical specialists are committed to ensuring the well-being of every patient through advanced treatment, personalized care, and a patient-first approach.
+        </p>
       </div>
-      
-      <div className='flex '>
-      {/*left*/}
-      <div className='flex-col ml-20 mt-14  '>
-        <p className='text-gray-700 text-4xl font-bold'>Commited to Excellence in HealthCare</p>
-        <div className='max-w-2xl '>
-            <p className='mt-8 text-gray-700 text-justify leading-relaxed'>At Grand Bishoftu Hospital, we are committed to providing exceptional healthcare with compassion and expertise. Our experienced medical specialists work tirelessly to ensure the well-being of every patient, offering advanced treatments, personalized care, and a patient-first approach. </p>
-            <p className='mt-8 text-gray-700 text-justify leading-relaxed'>We strive to create a safe and welcoming environment where health, comfort, and trust come first.Providing trusted healthcare services with compassion and excellence. Our experienced medical specialists are committed to ensuring the well-being of every patient through advanced treatment, personalized care, and a patient-first approach</p>
-        </div>
 
-        <div className='mt-8 flex item center gap-10 max-w-3xl p-6 bg-white rounded-lg shadow-md'>
-            <p className='text-sky-400 text-4xl font-bold'>25+</p>
-            <p className='text-sky-400 text-4xl font-bold'>3000</p>
-        </div>
-         
-      </div>
-           
-        {/*right*/}
-        <div>
-           <img className="ml-8 w-140 mr-14 mt-14 rounded-2xl" src={hospital}/>
-        </div>
-        </div>
-        
-        
-    </div>
+    <div className='my-10 flex flex-col md:flex-row gap-10 items-center md:items-start'>
+  {/* Image */}
+  <img className='w-full md:max-w-[450px] rounded-2xl' src={hospital} alt=''/>
+  
+  {/* Text */}
+  <div className='flex-1 flex flex-col justify-center gap-6 text-gray-600'>
+    <p>
+      At Grand Bishoftu Hospital, we are committed to providing exceptional healthcare with compassion and expertise. Our experienced medical specialists work tirelessly to ensure the well-being of every patient, offering advanced treatments, personalized care, and a patient-first approach.
+    </p>
+    <p>
+      We strive to create a safe and welcoming environment where health, comfort, and trust come first. Providing trusted healthcare services with compassion and excellence. Our experienced medical specialists are committed to ensuring the well-being of every patient through advanced treatment, personalized care, and a patient-first approach.
+    </p>
+  </div>
+</div>
+
+
+
+
+
+</div>
+
 
     
   )
