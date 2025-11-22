f02a9b3b9c9b63298e06c4d75bc67502695e1dba
Tsedniya <ktsedniya@gmail.com>
2025-11-22 21:08:47 +0300

    frontend:hero

--- myapp/src/components/Hero.jsx
+++ myapp/src/components/Hero.jsx
@@ -15,33 +15,27 @@ const Hero = () => {
   
         <div className="relative z-10 flex flex-col justify-between min-h-screen px-6 md:px-16 lg:px-24 pt-65 pb-50">
 
-        {/* Top content */}
-        <div className="flex flex-col items-start gap-6 md:gap-8">
-          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white leading-tight">
-            Grand Bishoftu Hospital
-          </h1>
+        
+      <div className="flex flex-col items-start gap-6 md:gap-8">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white leading-tight">
+              Grand Bishoftu Hospital
+            </h1>
 
-          <p className="text-sm sm:text-base md:text-lg text-white max-w-xl md:max-w-2xl">
-            Providing trusted healthcare services with compassion and excellence. Our experienced medical specialists are committed to ensuring the well-being of every patient through advanced treatment and personalized care.
-          </p>
+            <p className="text-sm sm:text-base md:text-lg text-white max-w-xl md:max-w-2xl">
+              Providing trusted healthcare services with compassion and excellence. Our experienced medical specialists are committed to ensuring the well-being of every patient through advanced treatment and personalized care.
+            </p>
 
-          <div className="flex flex-col sm:flex-row gap-3 sm:gap-4">
-            <button className="font-bold px-6 py-3 bg-sky-400 text-white rounded-full hover:bg-sky-700 hover:cursor-pointer">
-              Learn More
-            </button>
-            <button className="font-bold px-6 py-3 border border-sky-400 text-white rounded-full hover:bg-sky-400 hover:cursor-pointer">
-              Explore Services
-            </button>
-          </div>
-
-         {/* <div className="flex justify-center mt-10">
-              <div className="flex flex-col sm:flex-row gap-4 border border-amber-600 py-5 px-6 bg-black/40 text-white rounded-xl">
-                <p>Cardiology</p>
-                <p>Pulmonogy</p>
-                <p>Diagnostics</p>
-              </div>*/}
+            <div className="flex flex-col sm:flex-row gap-3 sm:gap-4">
+              <button className="font-bold px-6 py-3 bg-sky-400 text-white rounded-full hover:bg-sky-700 hover:cursor-pointer">
+                Learn More
+              </button>
+              <button className="font-bold px-6 py-3 border border-sky-400 text-white rounded-full hover:bg-sky-400 hover:cursor-pointer">
+                Explore Services
+              </button>
+            </div>
         </div>
-    </div>
+
+        </div>
 
     </section>
   );
