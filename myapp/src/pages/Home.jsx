4ec3869e670d4c0c96df34d41b81019e006b9c05
Tsedniya <ktsedniya@gmail.com>
2025-11-24 17:20:34 +0300

    frontend:footer

--- myapp/src/pages/Home.jsx
+++ myapp/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
-//import TitleAbout from '../components/TitleAbout'
+import TitleAbout from '../components/TitleAbout'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <TitleAbout/>
     </div>
   )
 }
