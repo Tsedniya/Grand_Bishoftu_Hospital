2492b6653e79f63fbe5f89d897671c8c55f4be05
Tsedniya <ktsedniya@gmail.com>
2025-11-27 23:29:47 +0300

    frontend:homeone

--- myapp/src/components/Homeone.jsx
+++ myapp/src/components/Homeone.jsx
@@ -5,8 +5,11 @@ const Homeone = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] my-10 ' >
         {/*Left*/}
-        <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
-           <img className='rounded-2xl transition-transform duration-300 hover:scale-90' src={opration} alt=""/>
+        <div className='relative w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+           <img className='rounded-2xl' src={opration} alt=""/>
+           <div className='absolute right-0 bottom-0 bg-sky-400 z-50 h-50 text-gray-700 font-semibold'>
+             <p>25+</p>
+           </div>
         </div>
 
 
