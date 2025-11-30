ede7c52512d0a3bee2ff3ad985f6aaa98205b2dc
Tsedniya <ktsedniya@gmail.com>
2025-11-30 19:18:00 +0300

    frontend:departement

--- myapp/src/components/Departements.jsx
+++ myapp/src/components/Departements.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import patient from '../assets/patient.jpg'
+import pulse from '../assets/pulse.svg'
 
 const Departements = () => {
   return (
-   <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-12 px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] ">
+   <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-12 px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] gap-15">
      
      
         <div className='relative bg-white border w-80 h-100 rounded-2xl border-sky-400 mb-5 transition-transform duration-300 hover:scale-105 shadow-md'>
-    <img src={patient} className='relative h-35 w-100 object-cover rounded-t-xl' />
+         <img src={patient} className='relative h-35 w-100 object-cover rounded-t-xl' />
 
-    <div className="rounded-full bg-[#3DACD1] w-14 h-14 absolute top-[120px] right-10"></div>
+          <div class="relative">
+           <div class="rounded-full bg-[#3DACD1] w-14 h-14 absolute top-[-30px] left-10  flex items-center justify-center overflow-hidden">
+            <img src={pulse} class="w-11 h-11 object-cover" />
+          </div>
+         </div>
 
-    <p className='mt-5'>Cardiology</p>
+
+    <p className='mt-12'>Cardiology</p>
     <p>Comprehensive cardiovascular care with advanced diagnostic techniques and treatment options for heart conditions, ensuring optimal cardiac health for all patients.</p>
     </div>
 
