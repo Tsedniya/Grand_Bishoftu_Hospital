fcf26f692d58b935bd6ffab1114afe69c690e9aa
Tsedniya <ktsedniya@gmail.com>
2025-11-30 21:49:53 +0300

    frontend:department

--- myapp/src/components/Departements.jsx
+++ myapp/src/components/Departements.jsx
@@ -6,18 +6,22 @@ import bone from '../assets/bone.svg'
 import baby from '../assets/baby.svg'
 import ambulance from '../assets/ambulance.svg'
 import shield from '../assets/shield.svg'
-
+import photo_1 from '../assets/photo_1.jpg'
+import photo_2 from '../assets/photo_2.jpg'
+import photo from '../assets/photo.jpg'
+import men from '../assets/men.jpg'
+import cancer from '../assets/cancer.jpg'
 
 const Departements = () => {
   return (
     
-   <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-12 px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] gap-15">
+   <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-12 px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] ">
      
     
     <div className='relative bg-white border w-80 h-100 rounded-2xl border-white mb-5 transition-transform duration-300 hover:scale-105 shadow-md'>
          <img src={patient} className='relative h-35 w-100 object-cover rounded-t-xl' />
 
-          <div class="relative">
+          <div className="relative">
            <div className="rounded-full bg-[#3DACD1] w-14 h-14 absolute top-[-30px] left-10  flex items-center justify-center overflow-hidden">
             <img src={pulse} className="w-8 h-8 object-cover" />
           </div>
@@ -28,10 +32,10 @@ const Departements = () => {
          <p className='mt-3 px-7 text-justify text-gray-700'>Comprehensive cardiovascular care with advanced diagnostic techniques and treatment options for heart conditions, ensuring optimal cardiac health for all patients.</p>
     </div>
 
-     <div className='relative bg-white border w-80 h-100 rounded-2xl border-sky-400 mb-5 transition-transform duration-300 hover:scale-105 shadow-md'>
-         <img src={patient} className='relative h-35 w-100 object-cover rounded-t-xl' />
+     <div className='relative bg-white border w-80 h-100 rounded-2xl border-white mb-5 transition-transform duration-300 hover:scale-105 shadow-md'>
+         <img src={photo} className='relative h-35 w-100 object-cover rounded-t-xl' />
 
-          <div class="relative">
+          <div className="relative">
            <div className="rounded-full bg-[#3DACD1] w-14 h-14 absolute top-[-30px] left-10  flex items-center justify-center overflow-hidden">
             <img src={brain} className="w-8 h-8 object-cover" />
           </div>
@@ -42,10 +46,10 @@ const Departements = () => {
          <p className='mt-3 px-7 text-justify text-gray-700'>Expert neurological care specializing in brain and nervous system disorders, providing cutting-edge treatments and compassionate support for neurological conditions.</p>
     </div>
 
-     <div className='relative bg-white border w-80 h-100 rounded-2xl border-sky-400 mb-5 transition-transform duration-300 hover:scale-105 shadow-md'>
-         <img src={patient} className='relative h-35 w-100 object-cover rounded-t-xl' />
+     <div className='relative bg-white border w-80 h-100 rounded-2xl border-white mb-5 transition-transform duration-300 hover:scale-105 shadow-md'>
+         <img src={photo_1} className='relative h-35 w-100 object-cover rounded-t-xl' />
 
-          <div class="relative">
+          <div className="relative">
            <div className="rounded-full bg-[#3DACD1] w-14 h-14 absolute top-[-30px] left-10  flex items-center justify-center overflow-hidden">
             <img src={bone} className="w-8 h-8 object-cover" />
           </div>
@@ -56,10 +60,10 @@ const Departements = () => {
          <p className='mt-3 px-7 text-justify text-gray-700'>Advanced musculoskeletal care focusing on bones, joints, and muscles with innovative surgical and non-surgical treatment approaches for mobility restoration.</p>
     </div>
 
-     <div className='relative bg-white border w-80 h-100 rounded-2xl border-sky-400 mb-5 transition-transform duration-300 hover:scale-105 shadow-md'>
-         <img src={patient} className='relative h-35 w-100 object-cover rounded-t-xl' />
+     <div className='relative bg-white border w-80 h-100 rounded-2xl border-white mb-5 transition-transform duration-300 hover:scale-105 shadow-md'>
+         <img src={photo_2} className='relative h-35 w-100 object-cover rounded-t-xl' />
 
-          <div class="relative">
+          <div className="relative">
            <div className="rounded-full bg-[#3DACD1] w-14 h-14 absolute top-[-30px] left-10  flex items-center justify-center overflow-hidden">
             <img src={baby} className="w-8 h-8 object-cover" />
           </div>
@@ -70,10 +74,10 @@ const Departements = () => {
          <p className='mt-3 px-7 text-justify text-gray-700'>Specialized healthcare for children from infancy through adolescence, offering comprehensive medical care in a child-friendly environment with experienced pediatric specialists.</p>
     </div>
 
-   <div className='relative bg-white border w-80 h-100 rounded-2xl border-sky-400 mb-5 transition-transform duration-300 hover:scale-105 shadow-md'>
-         <img src={patient} className='relative h-35 w-100 object-cover rounded-t-xl' />
+   <div className='relative bg-white border w-80 h-100 rounded-2xl border-white mb-5 transition-transform duration-300 hover:scale-105 shadow-md'>
+         <img src={cancer} className='relative h-35 w-100 object-cover object-center rounded-t-xl' />
 
-          <div class="relative">
+           <div className="relative">
            <div className="rounded-full bg-[#3DACD1] w-14 h-14 absolute top-[-30px] left-10  flex items-center justify-center overflow-hidden">
             <img src={shield} className="w-8 h-8 object-cover" />
           </div>
@@ -85,10 +89,10 @@ const Departements = () => {
           .</p>
      </div>
 
-      <div className='relative bg-white border w-80 h-100 rounded-2xl border-sky-400 mb-5 transition-transform duration-300 hover:scale-105 shadow-md'>
-         <img src={patient} className='relative h-35 w-100 object-cover rounded-t-xl' />
+      <div className='relative bg-white border w-80 h-100 rounded-2xl border-white mb-5 transition-transform duration-300 hover:scale-105 shadow-md'>
+         <img src={men} className='relative h-35 w-100 object-cover rounded-t-xl' />
 
-          <div class="relative">
+          <div className="relative">
            <div className="rounded-full bg-[#3DACD1] w-14 h-14 absolute top-[-30px] left-10  flex items-center justify-center overflow-hidden">
             <img src={ambulance} className="w-8 h-8 object-cover" />
           </div>
