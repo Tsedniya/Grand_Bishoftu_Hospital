c8c8db4256d12023cd1196561e7bda9cfa6ac82c
Tsedniya <ktsedniya@gmail.com>
2025-12-02 10:29:05 +0300

    frontend:about

--- myapp/src/components/Hero.jsx
+++ myapp/src/components/Hero.jsx
@@ -3,45 +3,48 @@ import home from "../assets/home.jpg";
 
 const Hero = () => {
   return (
-     <section
-            className="
-              relative w-full h-screen bg-no-repeat bg-cover 
-              bg-[center_50%]        /* mobile/tablet */
-              lg:bg-[center_85%]     /* desktop */
-            "
-            style={{ backgroundImage: `url(${home})` }}
-          >
+    <div className="flex flex-col h-screen">
+      {/* Hero image */}
+      <section
+        className="relative flex-1 w-full"
+        style={{
+          backgroundImage: `url(${home})`,
+          backgroundPosition: "center 70%",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat" ,
+        }}
+      >
+        {/* Gradient */}
+        <div className="absolute inset-0 bg-gradient-to-r from-sky-950/90 via-sky-900/35 to-white/0" />
 
+        {/* Content on image */}
+        <div className="relative z-10 flex flex-col justify-center h-full mt-10 px-6 md:px-16 lg:px-24">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-poppins font-bold text-white leading-tight">
+            Grand Bishoftu Hospital
+          </h1>
 
-        <div className="absolute inset-0 bg-gradient-to-t from-sky-950/50 via-sky-900/35 to-transparent" />
-
-
-    
-          <div className="relative z-10 flex flex-col justify-between min-h-screen px-6 md:px-16 lg:px-24 pt-65 pb-50">
-
-          
-          <div className="flex flex-col items-start gap-6 md:gap-8">
-              <h1 className="text-3xl sm:text-4xl md:text-5xl font-poppins font-bold text-white leading-tight">
-                Grand Bishoftu Hospital
-              </h1>
-
-          <p className="text-base sm:text-lg md:text-xl font-opensans text-white max-w-full md:max-w-2xl text-left md:text-justify leading-relaxed text-justify-inter-word">
-            Providing trusted healthcare services with compassion and excellence.Our experienced medical specialists are committed to ensuring the well-being of every patient through advanced treatment and personalized care.
+          <p className="text-base sm:text-lg md:text-xl font-opensans text-white max-w-full md:max-w-2xl leading-relaxed mt-4">
+            Providing trusted healthcare services with compassion and excellence.
+            Our experienced medical specialists are committed to ensuring the
+            well-being of every patient through advanced treatment and
+            personalized care.
           </p>
 
-              <div className="flex flex-col sm:flex-row gap-3 sm:gap-4">
-                <button className="font-bold font-opensans px-6 py-3 bg-sky-400 text-white rounded-full hover:bg-sky-700 hover:cursor-pointer">
-                  Learn More
-                </button>
-                <button className="font-bold font-opensans px-6 py-3 border border-sky-400 text-white rounded-full hover:bg-sky-400 hover:cursor-pointer">
-                  Explore Services
-                </button>
-              </div>
-           </div>
-
+          <div className="flex flex-col sm:flex-row gap-3 sm:gap-4 mt-6">
+            <button className="font-bold font-opensans px-6 py-3 bg-sky-400 text-white rounded-full hover:bg-sky-700">
+              Learn More
+            </button>
+            <button className="font-bold font-opensans px-6 py-3 border border-sky-400 text-white rounded-full hover:bg-sky-400">
+              Explore Services
+            </button>
+          </div>
         </div>
+      </section>
 
-    </section>
+      
+    </div>
+
+
   );
 };
 
