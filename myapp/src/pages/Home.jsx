0995403fa8801d35e90ff063bcb65645fec25124
Tsedniya <ktsedniya@gmail.com>
2025-12-02 13:05:42 +0300

    frontend:updatetheabout

--- myapp/src/pages/Home.jsx
+++ myapp/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 //import TitleAbout from '../components/TitleAbout'
 import Homeone from '../components/Homeone'
-import Departements from '../components/Departements'
+//import Departements from '../components/Departements'
 
 
 const Home = () => {
@@ -10,7 +10,7 @@ const Home = () => {
     <div>
       <Hero />
       <Homeone/>
-      <Departements/>
+      {/*<Departements/>*/}
     </div>
   )
 }
